e3c7e3d28cc0396d6fc7cf5758930b2f41e2d67a
Diego Pardo Blacutt <dpardoblacutt@gmail.com>
2025-06-30 15:48:11 -0400

    Install Next and create boilerplate project structure

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <h1 className="text-3xl font-bold">Product Suggestion Wall</h1>
+    </main>
+  );
+}
